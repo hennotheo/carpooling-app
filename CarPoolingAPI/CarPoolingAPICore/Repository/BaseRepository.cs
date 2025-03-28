b68d65f7de30719f1b8ea1acc3b5a25351ddeff9
NazioLT <zoupeurletroll@gmail.com>
2025-03-28 11:08:47 +0100

    :recycle: Repo Delete

--- CarPoolingAPI/CarPoolingAPICore/Repository/BaseRepository.cs
+++ CarPoolingAPI/CarPoolingAPICore/Repository/BaseRepository.cs
@@ -1,17 +1,23 @@
-﻿using CarPoolingAPICore.Exceptions;
+﻿using System.Reflection;
+using CarPoolingAPICore.Exceptions;
+using CarPoolingAPICore.Extensions;
 using CarPoolingAPICore.Interface;
 
 namespace CarPoolingAPICore.Repository;
 
 public class BaseRepository<TId, T> : IRepository<TId, T>
 {
-    private List<T> _entities;//TODO: Change the variable
-    
+    private List<T> _entities; //TODO: Change the variable
+
+    private readonly PropertyInfo _idProp;
+
     protected virtual IList<T> Entities => _entities;
 
     public BaseRepository()
     {
         _entities = new List<T>();
+
+        _idProp = typeof(T).GetProperty("Id")!;
     }
 
     public async Task<IEnumerable<T>> GetAll()
@@ -22,12 +28,12 @@ public class BaseRepository<TId, T> : IRepository<TId, T>
     public async Task<T> GetById(TId id)
     {
         T? value = await Task.Run(() => Entities
-            .FirstOrDefault(e => e.GetType().GetProperty("Id").GetValue(e).Equals(id)));
+            .FirstOrDefault(entity => _idProp.PropertyEquals(entity, id)));
 
-        if (object.Equals(value, default(T)))
+        if (Equals(value, default(T)))
             throw new RepoDataNotFoundException();
 
-        return value;
+        return value!;
     }
 
     public async Task Add(T entity)
@@ -43,7 +49,7 @@ public class BaseRepository<TId, T> : IRepository<TId, T>
     public async Task Delete(TId id)
     {
         T entity = await GetById(id);
-        
+
         await Task.Run(() => Entities.Remove(entity));
     }
 }
