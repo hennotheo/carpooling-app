5925937b760386682fd45bc64a7bcb9a92d0346d
NazioLT <zoupeurletroll@gmail.com>
2025-04-04 15:49:36 +0200

    :test_tube: Test Jwt Generation

---
+++ CarPoolingAPI/Tests_CarPoolingAPI/Jwt_Tests.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+
+namespace Tests_CarPoolingAPI;
+
+[TestFixture(Category = "Authentication")]
+public class Jwt_Tests
+{
+    private HttpClient _client;
+
+    [SetUp]
+    public virtual void Setup()
+    {
+        var factory = new WebApplicationFactory<Program>();
+        _client = factory.CreateClient();
+    }
+
+    [TearDown]
+    public virtual void TearDown()
+    {
+        _client.Dispose();
+    }
+
+    [Test]
+    public async Task GenerateJwtToken_ShouldReturnToken()
+    {
+        var loginModel = new { Username = "testuser", Password = "password" };
+        var content = new StringContent(JsonConvert.SerializeObject(loginModel), Encoding.UTF8, "application/json");
+        
+        var response = await _client.PostAsync("/api/auth/login", content);
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        
+        Assert.That(responseString, Is.Not.Null);
+        Assert.That(responseString.Contains("token"), Is.True);
+    }
+}
