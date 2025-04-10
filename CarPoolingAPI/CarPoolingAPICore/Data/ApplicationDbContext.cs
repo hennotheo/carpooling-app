edcf4c27a059a02a074fe967d87a82f9232d7b4d
NazioLT <zoupeurletroll@gmail.com>
2025-04-10 13:23:44 +0200

    :heavy_plus_sign: Add Efcore

---
+++ CarPoolingAPI/CarPoolingAPICore/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using CarPoolingAPICore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CarPoolingAPICore.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public DbSet<User> Users { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration) : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\car;Integrated Security=True;");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
--- CarPoolingAPI/CarPoolingAPICore/Models/User.cs
+++ CarPoolingAPI/CarPoolingAPICore/Models/User.cs
@@ -1,11 +1,19 @@
-﻿namespace CarPoolingAPICore.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarPoolingAPICore.Models;
 
 public class User
 {
     public int Id { get; set; }
+
+    [Required, MaxLength(20), MinLength(3)]
     public string FirstName { get; set; }
-    public string LastName { get; set; }
+
+    [MaxLength(20)] public string LastName { get; set; }
+
+    [Required, EmailAddress, MaxLength(100)]
     public string Email { get; set; }
-    
+
+    [Required] 
     public string HashedPassword { get; set; }
 }
